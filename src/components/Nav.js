ec938a9e3b3f740f5a28ebb9606daed344ad76c3
Alex <alexandruzmau320@gmail.com>
2022-09-16 09:39:21 +0300

    Install material-ui icons / Change nav link's styles

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,33 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import navStyles from '../styles/nav.module.css';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 
 const Nav = () => {
+  const [activeLink, setActiveLink] = useState(0);
+
   return (
     <nav>
       <div className={navStyles.logo}>Fake Shop</div>
       <div className={navStyles.links}>
-        <Link to="/">Home</Link>
-        <Link to="/shop">Shop</Link>
+        <NavLink
+          onClick={() => setActiveLink(0)}
+          className={activeLink === 0 ? navStyles.activeLink : ''}
+          to="/"
+        >
+          Home
+        </NavLink>
+        <NavLink
+          onClick={() => setActiveLink(1)}
+          className={activeLink === 1 ? navStyles.activeLink : ''}
+          to="/shop"
+        >
+          Shop
+        </NavLink>
       </div>
-      <button className={navStyles.cart}>Cart</button>
+      <button className={navStyles.cart}>
+        <ShoppingBasketIcon />
+      </button>
     </nav>
   );
 };
